55d0880225a207aa2250cabd2891b4d0e90a40de
Emil Woźniak <emil.wozniak.591986@gmail.com>
2021-03-11 13:13:21 +0100

    validator

--- src/main/kotlin/pl/emil/microservices/web/handlers/RequestValidator.kt
+++ src/main/kotlin/pl/emil/microservices/web/handlers/RequestValidator.kt
@@ -3,26 +3,24 @@ package pl.emil.microservices.web.handlers
 import org.springframework.stereotype.Component
 import org.springframework.validation.Errors
 import org.springframework.validation.SmartValidator
-import org.springframework.validation.ValidationUtils.rejectIfEmptyOrWhitespace
-import org.springframework.validation.Validator
-import pl.emil.microservices.model.Post
-import pl.emil.microservices.model.user.User
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
-
-const val FIELD_REQUIRED = "field.required"
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter
+import pl.emil.microservices.web.validator.LocalValidator
 
 @Component
-class RequestValidator : SmartValidator {
+class RequestValidator(private val validator: LocalValidator) : SmartValidator {
     override fun supports(clazz: Class<*>): Boolean = RequestValidator::class.java.isAssignableFrom(clazz)
 
     override fun validate(target: Any, errors: Errors, vararg validationHints: Any?) {
-        validationHints.forEach {
-            rejectIfEmptyOrWhitespace(errors, it.toString(), FIELD_REQUIRED)
-        }
+        this.validate(target, errors)
+//        validationHints.forEach {
+//            rejectIfEmptyOrWhitespace(errors, it.toString(), FIELD_REQUIRED)
+//        }
     }
 
     override fun validate(target: Any, errors: Errors) {
-        rejectIfEmptyOrWhitespace(errors, "title", FIELD_REQUIRED)
-        rejectIfEmptyOrWhitespace(errors, "content", FIELD_REQUIRED)
+        val validatorAdapter = SpringValidatorAdapter(validator)
+        validatorAdapter.validate(target, errors)
+//        rejectIfEmptyOrWhitespace(errors, "title", FIELD_REQUIRED)
+//        rejectIfEmptyOrWhitespace(errors, "content", FIELD_REQUIRED)
     }
 }
