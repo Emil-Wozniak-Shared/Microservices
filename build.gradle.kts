d18a0ec2e01449c551f186240c6feced000f9334
Emil Woźniak <emil.wozniak.591986@gmail.com>
2021-03-12 14:40:46 +0100

    extras

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,36 +35,28 @@ repositories {
     mavenCentral()
 }
 
-val LOGBACK_VERSION = "6.4"
-val GROOVY_VERSION = "3.0.7"
-val OBJENESIS_VERSION = "org.objenesis:objenesis:3.1"
-val CLIB_VERSION = "cglib:cglib-nodep:3.3.0"
-val GROOVY = "org.codehaus.groovy:groovy:$GROOVY_VERSION"
-val GROOVY_VERSION_ALL = "org.codehaus.groovy:groovy-all:$GROOVY_VERSION"
-val SPOCK_VERSION = "2.0-M4-groovy-3.0"
-val SPOCK_CORE_VERSION = "org.spockframework:spock-core:$SPOCK_VERSION"
-val SPOCK_SPRING_VERSION = "org.spockframework:spock-spring:$SPOCK_VERSION"
-val SPOCK_BOM = "org.spockframework:spock-bom:$SPOCK_VERSION"
-val BYTEBUDDY_VERSION = "net.bytebuddy:byte-buddy:1.10.14"
-val HAMCREST_VERSION = "org.hamcrest:hamcrest-core:2.2"
-
-configurations {
-    all {
-        resolutionStrategy {
-            eachDependency {
-                if (requested.group == "org.codehaus.groovy") {
-                    useVersion("3.0.7")
-                }
-            }
-        }
-    }
-}
+extra["LOGBACK_VERSION"] = "6.4"
+extra["GROOVY_VERSION"] = "3.0.7"
+extra["OBJENESIS_VERSION"] = "org.objenesis:objenesis:3.1"
+extra["CLIB_VERSION"] = "cglib:cglib-nodep:3.3.0"
+extra["GROOVY"] = "org.codehaus.groovy:groovy:${properties["GROOVY_VERSION"]}"
+extra["GROOVY_VERSION_ALL"] = "org.codehaus.groovy:groovy-all:${properties["GROOVY_VERSION"]}"
+extra["SPOCK_VERSION"] = "2.0-M4-groovy-3.0"
+extra["SPOCK_CORE_VERSION"] = "org.spockframework:spock-core:${properties["SPOCK_VERSION"]}"
+extra["SPOCK_SPRING_VERSION"] = "org.spockframework:spock-spring:${properties["SPOCK_VERSION"]}"
+extra["SPOCK_BOM"] = "org.spockframework:spock-bom:${properties["SPOCK_VERSION"]}"
+extra["BYTEBUDDY_VERSION"] = "net.bytebuddy:byte-buddy:1.10.14"
+extra["HAMCREST_VERSION"] = "org.hamcrest:hamcrest-core:2.2"
+extra["springCloudVersion"] = "2020.0.1"
+extra["JACKSON_VERSION"] = "2.12.2"
 
 dependencies {
-    implementation("net.logstash.logback:logstash-logback-encoder:${LOGBACK_VERSION}")
+    implementation("net.logstash.logback:logstash-logback-encoder:${properties["LOGBACK_VERSION"]}")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.data:spring-data-commons")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -72,11 +64,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation("com.fasterxml.jackson.core:jackson-core:2.12.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.2")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:${properties["JACKSON_VERSION"]}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${properties["JACKSON_VERSION"]}")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:${properties["JACKSON_VERSION"]}")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:${properties["JACKSON_VERSION"]}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${properties["JACKSON_VERSION"]}")
     implementation("javax.validation:validation-api:2.0.1.Final")
     implementation("javax.servlet:javax.servlet-api:4.0.1")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
@@ -95,17 +87,34 @@ dependencies {
     implementation("io.r2dbc:r2dbc-spi:0.9.0.M1")
     implementation("io.r2dbc:r2dbc-postgresql:0.8.6.RELEASE")
 
-    implementation(GROOVY_VERSION_ALL)
-    implementation(GROOVY)
-    testImplementation(SPOCK_CORE_VERSION)
-    testImplementation(SPOCK_SPRING_VERSION)
-    testImplementation(platform(SPOCK_BOM))
-    implementation(GROOVY_VERSION_ALL)
-    testImplementation(CLIB_VERSION)
-    testImplementation(HAMCREST_VERSION) // only necessary if Hamcrest matchers are used
-    testImplementation(BYTEBUDDY_VERSION)
-    testImplementation(OBJENESIS_VERSION)
+    implementation(  "${properties["GROOVY_VERSION_ALL"]}")
+    implementation("${properties["GROOVY"]}")
+    testImplementation("${properties["SPOCK_CORE_VERSION"]}")
+    testImplementation("${properties["SPOCK_SPRING_VERSION"]}")
+    testImplementation(platform("${properties["SPOCK_BOM"]}"))
+    implementation("${properties["GROOVY_VERSION_ALL"]}")
+    testImplementation("${properties["CLIB_VERSION"]}")
+    testImplementation("${properties["HAMCREST_VERSION"]}")
+    testImplementation("${properties["BYTEBUDDY_VERSION"]}")
+    testImplementation("${properties["OBJENESIS_VERSION"]}")
+}
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+configurations {
+    all {
+        resolutionStrategy {
+            eachDependency {
+                if (requested.group == "org.codehaus.groovy") {
+                    useVersion("3.0.7")
+                }
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
