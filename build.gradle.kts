b269788cb73a617eb09d1bf7a4749a65c2fc46b2
Emil Woźniak <emil.wozniak.591986@gmail.com>
2021-03-15 14:26:03 +0100

    solve gateway issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.glassfish.jaxb:jaxb-runtime")
 }
 
 dependencyManagement {
