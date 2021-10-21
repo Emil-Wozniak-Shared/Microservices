90eb4ab74f0edbed331666076a1a03b2dfdccd0a
Emil Woźniak <emil.wozniak.591986@gmail.com>
2021-10-21 18:27:15 +0200

    - change addCommon gradle.build files in each project

--- pdf/build.gradle.kts
+++ pdf/build.gradle.kts
@@ -14,9 +14,15 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    mavenLocal()
+}
+
+fun DependencyHandler.kotest(target: String = "", module: String = "", version: String = "4.6.3") {
+    this.testImplementation("io.kotest${if (module.isBlank()) "" else ".$module"}:kotest-$target:$version")
 }
 
 dependencies {
+    implementation ("pl.emil:contract:1.0.0")
     implementation("org.codehaus.groovy:groovy-all:3.0.8")
     implementation("net.sourceforge.htmlcleaner:htmlcleaner:2.25")
     implementation("org.apache.xmlgraphics:fop:2.6")
@@ -26,9 +32,21 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.xhtmlrenderer:flying-saucer-core:9.0.7")
+    implementation("org.xhtmlrenderer:flying-saucer-pdf:9.0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("com.codeborne:pdf-test:1.6.1")
+    testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+    kotest("assertions-core")
+    kotest("property")
+    kotest("runner-junit5")
+    kotest("assertions-core-jvm")
+    kotest("framework-engine-jvm")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.0.1")
+    testImplementation ("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.apache.pdfbox:pdfbox:2.0.24")
 }
 
 tasks.named<AbstractCompile>("compileGroovy") {
